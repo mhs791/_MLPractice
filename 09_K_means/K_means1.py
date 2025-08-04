f536fe6dc1f5ee19c4468454e139aec2faa2cced
mhs791 <mistersoleimani@gmail.com>
2025-08-05 00:26:35 +0330

    18th

--- 09_K_means/K_means1.py
+++ 09_K_means/K_means1.py
@@ -2,13 +2,18 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-x, y = make_blobs(n_samples=300, centers=3, random_state=42)
+X, y_true = make_blobs(n_samples=300, centers=3, random_state=42)
 
-model = KMeans(n_clusters=3, random_state=42)
-model.fit(x)
+kmeans = KMeans(n_clusters=3, random_state=42)
+kmeans.fit(X)
 
-centers = model.cluster_centers_
+labels = kmeans.labels_
+centers = kmeans.cluster_centers_
 
-plt.scatter(x[:, 0], x[:, 1], c=model.labels_)
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', s=50)
 plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='x', s=200)
+plt.title('KMeans Clustering')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.grid(True)
 plt.show()
